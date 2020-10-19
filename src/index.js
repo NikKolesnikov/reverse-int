ba0c0d936758a148bb20bb26a31c9897a484f336
NikKolesnikov <nikkolesnikov94@gmail.com>
2020-10-19 21:24:11 +0300

    add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    n = String(n);
+
+    let result = '';
+
+    if (n[0] === '-') {
+        result = n.split('').reverse().slice(0, -1).join('');
+    } else {
+        result = n.split('').reverse().join('');
+    }
+
+    return result;
 }
